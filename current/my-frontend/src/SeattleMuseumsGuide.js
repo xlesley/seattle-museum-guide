619eb35155767d067a11c0add61fc27c51ee5e1d
irenehuonline <irenehuonline@gmail.com>
2024-05-16 01:16:20 -0700

    set up 'bottom navigation' tabs

--- current/my-frontend/src/SeattleMuseumsGuide.js
+++ current/my-frontend/src/SeattleMuseumsGuide.js
@@ -1,11 +1,37 @@
-import Typography from "@mui/material/Typography";
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+function BottomNav(props) {
+  // Define state for the value of the BottomNavigation
+  const [value, setValue] = useState(0);
+
+  return (
+    <BottomNavigation
+      showLabels
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <BottomNavigationAction label="Home" /*icon={<SmthIcon />}*/ />
+      <BottomNavigationAction label="Exhibitions" /*icon={<SmthIcon />}*//>
+      <BottomNavigationAction label="Favorites" /*icon={<SmthIcon />}*//>
+      <BottomNavigationAction label="Profile" /*icon={<SmthIcon />}*//>
+    </BottomNavigation>
+  );
+}
 
 function SeattleMuseumsGuide(props) {
-    return (
-        <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
-            Label Mapper
-        </Typography>
-    );
-  }
-  
-  export default SeattleMuseumsGuide;
+  return (
+    <>
+      <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
+        Seattle Museums Guide
+      </Typography>
+      <BottomNav />
+    </>
+  );
+}
+
+export default SeattleMuseumsGuide;

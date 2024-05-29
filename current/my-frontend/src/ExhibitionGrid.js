78af1522e34501bce288b147d4855ac8f567bbf7
xlesley <114975168+xlesley@users.noreply.github.com>
2024-05-29 10:17:44 -0700

    Fixed bottom padding for exhibitions

--- current/my-frontend/src/ExhibitionGrid.js
+++ current/my-frontend/src/ExhibitionGrid.js
@@ -17,7 +17,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function ExhibitionGrid ({ exhibitions }) {
     return (
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} paddingBottom={10}>
             {exhibitions.map((exhibition, index) => (
                 <Grid item xs={12} sm={4} md={4} key={index}>
                     <Card >
